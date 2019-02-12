830bfed02b384fadc40aa06dfe551bab8dd2086e
Jonathan Lin <jlin1999@gmail.com>
2019-02-11 21:22:56 -0500

    Initial

---
+++ mtreeves808-laserturret-a0427eda3cfe/Laser Turret/MyLaserTurret/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace MyLaserTurret
+{
+	partial class main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.port = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // port
+            // 
+            this.port.PortName = "COM3";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 514);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main";
+            this.Text = "Laser Turret - Mouse Control";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.IO.Ports.SerialPort port;
+	}
+}
+
---
+++ mtreeves808-laserturret-a0427eda3cfe/arduino_code/arduino_code.ino
@@ -0,0 +1,40 @@
+#include<Servo.h>
+
+Servo servoX;
+Servo servoY;
+
+String serialData;
+
+void setup() 
+{
+
+  servoX.attach(6);
+  servoY.attach(7);
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+}
+
+void loop() 
+{
+  //lol
+}
+
+void serialEvent() 
+{
+  serialData = Serial.readString();
+  servoX.write(parseDataX(serialData));
+  servoY.write(parseDataY(serialData));
+}
+
+int parseDataX(String data)
+{
+  data.remove(data.indexOf("Y"));
+  data.remove(data.indexOf("X"), 1);
+  return data.toInt();
+}
+
+int parseDataY(String data)
+{
+  data.remove(0,data.indexOf("Y") + 1);
+  return data.toInt();
+}
